43caf4a362d73bc455f61e2cddfbababbc816b67
lgonzalez06 <luis.gc06@hotmail.com>
2016-04-21 00:29:04 -0500

    Mantenimento de Usuarios...

---
+++ DisiProject/Controllers/UsuarioController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DisiProject.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        //
+        // GET: /Usuario/
+
+        public ActionResult MttoUsuario()
+        {
+            ViewBag.titulo = "Mantenimiento de Usuaios";
+            return View();
+        }
+
+    }
+}
