dd5e193b4ed855a22e598ee9927046beea2be6a0
tonycrazy13 <luis.gc06@gmail.com>
2016-04-21 18:28:43 -0500

    ctcg -prueba

--- DisiProject/Controllers/HomeController.cs
+++ DisiProject/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace DisiProject.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your app description page.";
-
+             
             return View();
         }
 
